42656e1a598fe9a84c95b64df04869ecc3a25290
mendozamario <menriquemendoza@unicesar.edu.co>
2023-11-26 23:19:12 -0500

    Changed router file

--- proposal/proposal-project/src/app/app.routes.ts
+++ proposal/proposal-project/src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { ViewTeacherComponent } from './components/view-teacher/view-teacher.component';
+import { ViewStudentComponent } from './components/view-student/view-student.component';
+import { AddTeacherComponent } from './components/add-teacher/add-teacher.component';
+import { AddStudentComponent } from './components/add-student/add-student.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'view-teacher', component: ViewTeacherComponent },
+    { path: 'view-student', component: ViewStudentComponent },
+    { path: 'add-teacher', component: AddTeacherComponent },
+    { path: 'add-student', component: AddStudentComponent },
+];
